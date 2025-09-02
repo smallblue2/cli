0c29dc10e0246d4d7dfaba9e6ecd69b2b52db881
Niall Ryan <niallcryan@live.ie>
2025-09-02 16:38:20 +0100

    Implemented CLI parsing and tree traversal

--- src/cli.c
+++ src/cli.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include <allocators/arena.h>
+#include <collections/arena.h>
+#include <collections/parray.h>
 #include <string.h>
+#include <stdbool.h>
 
 void create_command() { printf("MEOW\n"); }
 
@@ -9,13 +11,13 @@ typedef struct cmd cmd_t;
 typedef enum { ACTION, GROUP } CMD_TYPE;
 
 typedef struct {
-  char **options;
-  char **args;
+  c_parray_t *options;
+  c_parray_t *flags;
   void *action;
 } cmd_action_t;
 
 typedef struct {
-  cmd_t *children; // null terminated list
+  c_parray_t *children;
 } cmd_group_t;
 
 struct cmd {
@@ -29,78 +31,189 @@ struct cmd {
   };
 };
 
-static alctr_arena_t *__CLI_TREE_ARENA;
+static c_arena_t *__CLI_TREE_ARENA;
+static c_parray_t *__CLI_PARRAY_TRACKER;
 
 int cli_print(cmd_t *cmd) {
   printf("%s: %s\n", cmd->name, cmd->desc);
   if (cmd->type == GROUP) {
-    for (cmd_t *cur_cmd = cmd->group->children; cur_cmd != NULL; cur_cmd++) {
-      printf("\t- %s: %s\n", cur_cmd->name, cur_cmd->desc);
+    c_parray_t *children = cmd->group->children;
+    for (int i = 0; i < parray_length(children); i++) {
+      cmd_t *child = (cmd_t*)parray_get(children, i);
+      if (child == NULL) {
+        printf("\t- NULL: NULL\n");
+      }
+      printf("\t- %s: %s\n", child->name, child->desc);
     }
   } else {
     printf("\tOPTIONS:\n");
-    for (char **option = cmd->action->options; option != NULL; option++) {
-      printf("\t\t%s\n", *option);
+    c_parray_t *options = cmd->action->options;
+    for (int i = 0; i < parray_length(options); i++) {
+      char *opt = (char*)parray_get(options, i);
+      if (opt == NULL) {
+        printf("\t\tNULL\n");
+      }
+      printf("\t\t%s\n", opt);
     }
     printf("\tFLAGS:\n");
-    for (char **flag = cmd->action->args; flag != NULL; flag++) {
-      printf("\t\t%s\n", *flag);
+    c_parray_t *flags = cmd->action->options;
+    for (int i = 0; i < parray_length(flags); i++) {
+      char *flag = (char*)parray_get(flags, i);
+      if (flag == NULL) {
+        printf("\t\tNULL\n");
+      }
+      printf("\t\t%s\n", flag);
     }
   }
   return 0;
 }
 
-cmd_t *add_to_group(cmd_t *child, cmd_t *parent) {
-  
-}
-
-cmd_t *create_group(const char *name) {
+cmd_t *create_group(const char *name, const char *desc) {
   cmd_t *new_cmd = (cmd_t*)arena_alloc(__CLI_TREE_ARENA, sizeof(cmd_t));
   if (new_cmd == NULL) return NULL;
   new_cmd->type = GROUP;
   new_cmd->name = name;
+  new_cmd->desc = desc;
   new_cmd->group = (cmd_group_t*)arena_alloc(__CLI_TREE_ARENA, sizeof(cmd_group_t));
   if (new_cmd->group == NULL) return NULL;
-  new_cmd->group->children = NULL;
+  new_cmd->group->children = parray_create();
+
+  // Add parray to global parray for easy cleanup
+
   return new_cmd;
 }
 
+// Returns false on error, true of success
+bool add_to_group(cmd_t *parent, cmd_t *child) {
+  if (parent->type != GROUP) {
+    return false;
+  }
+  parray_append(parent->group->children, child, sizeof(cmd_t));
+  return true;
+}
+
 // Can return NULL if the root fails to allocate on arena
-cmd_t *cli_init(char **argv) {
+cmd_t *cli_init(char **argv, const char *desc) {
   __CLI_TREE_ARENA = arena_create(1024);
-  return create_group(*argv);
+  if (__CLI_TREE_ARENA == NULL) exit(1);
+  __CLI_PARRAY_TRACKER = parray_create();
+  if (__CLI_PARRAY_TRACKER == NULL) exit(1);
+  return create_group(*argv, desc);
 }
 
 void cli_cleanup() {
+  for (int i = 0; i < parray_length(__CLI_PARRAY_TRACKER); i++) {
+    c_parray_t *parray = (c_parray_t*)parray_get(__CLI_PARRAY_TRACKER, i);
+    if (parray == NULL) exit(1);
+    parray_free(parray);
+  }
+  parray_free(__CLI_PARRAY_TRACKER);
   arena_free(__CLI_TREE_ARENA);
 }
 
-int cli_exec(cmd_t *root, int argc, char **argv) {
-  // We're at our destination
-  if (argc == 1) {
-    printf("GOT TO '%s', SHOULD DO ITS ACTION\n", root->name);
+typedef struct {
+  cmd_t *last_cmd_node;
+  c_parray_t *flags;
+  c_parray_t *options;
+  bool options_only;
+  int consumed;
+} __cmd_ctx_t;
+
+int __cli_exec_ctx(cmd_t *root, int argc, char **argv, __cmd_ctx_t *ctx) {
+  // Are we at the end?
+  if (ctx->consumed == argc) {
+finished:
+    if (root->type == GROUP) {
+      printf("FINISHED! [GROUP]\n\n");
+    } else {
+      // Execute something
+      printf("FINISHED! [ACTION]\n");
+      root->action->options = ctx->options;
+      root->action->flags = ctx->flags;
+    }
+
+    cli_print(root);
     return 0;
   }
 
-  // Figure out the next child to go to
-  if (root->type != GROUP) {
-    // Ideally print usage of the action
-    printf("USAGE: %s\n", root->name);
-    return 2;
+  const char *cur_str = argv[ctx->consumed];
+
+  // Are we in options only mode?
+  if (ctx->options_only) {
+    goto add_option;
   }
 
-  // Traverse through current group's children until we're empty, or we find a match
-  for (cmd_t *child = root->group->children; child != NULL && strcmp(child->name, *argv) != 0; child++) {
-    return cli_exec(child, argc - 1, argv + 1);
+  // Are we at a group?
+  if (root->type == GROUP) {
+    // Are we recursing deeper?
+    c_parray_t *children = root->group->children;
+    for (int i = 0; i < parray_length(children); i++) {
+      cmd_t *child = (cmd_t*)parray_get(children, i);
+      if (child == NULL) exit(1);
+      // If we find a match, go to it
+      if (strncmp(child->name, cur_str, sizeof(char) * (strlen(cur_str) + 1)) == 0) {
+        ctx->consumed++;
+        return __cli_exec_ctx(child, argc, argv, ctx);
+      }
+    }
+    // No match, ignore flags
+    printf("USAGE: xxxxx\n");
+    printf("FINISHED (shortcut)! [GROUP]\n"); // Print group info
+    goto finished;
   }
-  
-  printf("'%s' NOT IN GROUP '%s'\n", *argv, root->name);
-  return 2;
+
+  // Is the next item a flag?
+  if (*cur_str == '-') {
+    // If it is exactly "--", set the options only flag
+    if (strncmp(argv[ctx->consumed], "--", 3)) {
+      ctx->options_only = true;
+    } else {
+      // Consume all hyphens - just want to store the flag name
+      if (*(cur_str + 1) == '-') cur_str += 2;
+      else cur_str++;
+      parray_append(ctx->flags, cur_str, sizeof(char) * (strlen(cur_str) + 1));
+    }
+    ctx->consumed++;
+    return __cli_exec_ctx(root, argc, argv, ctx);
+  }
+
+  // Must be an option
+add_option:
+  parray_append(ctx->options, cur_str, sizeof(char) * (strlen(cur_str) + 1));
+  ctx->consumed++;
+  return __cli_exec_ctx(root, argc, argv, ctx);
+}
+
+int cli_exec(cmd_t *root, int argc, char **argv) {
+  __cmd_ctx_t *ctx = arena_alloc(__CLI_TREE_ARENA, sizeof(__cmd_ctx_t));
+  if (ctx == NULL) {
+    return 1;
+  }
+
+  // Starts at 1 as we know the first argument must be the binary name
+  ctx->consumed = 1;
+  ctx->options_only = false;
+  ctx->last_cmd_node = NULL;
+  ctx->flags = parray_create();
+  if (ctx->flags == NULL) return 1;
+  ctx->options = parray_create();
+  if (ctx->options == NULL) {
+    parray_free(ctx->flags);
+    return 1;
+  }
+
+  int result = __cli_exec_ctx(root, argc, argv, ctx);
+
+  parray_free(ctx->flags);
+  parray_free(ctx->options);
+
+  return result;
 }
 
 int main(int argc, char **argv) {
-  cmd_t *root = cli_init(argv);
-
+  cmd_t *root = cli_init(argv, "A test program");
+  cmd_t *meow = create_group("meow", "print a cat!");
+  add_to_group(root, meow);
   cli_exec(root, argc, argv);
   cli_cleanup();
   return 0;
